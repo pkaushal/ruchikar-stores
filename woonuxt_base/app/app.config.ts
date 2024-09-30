56abebfd3380a3c04a3ad10c73bb1bb75096ffb6
Prashant Kaushal <kaushal.prashant@gmail.com>
2024-09-30 21:02:03 +0530

    Update app.config.ts

--- woonuxt_base/app/app.config.ts
+++ woonuxt_base/app/app.config.ts
@@ -6,7 +6,7 @@
 export default defineAppConfig({
   siteName: 'WooNuxt',
   storeName: 'Ruchikar Stores',
-  shortDescription: 'Handpicked products for Them and you; at a price that you will love.',
+  shortDescription: 'Handpicked products for Them and you; at a price that you will love!',
   description: `Ruchikar Stores is a place where you can find handpicked products for you and your loved ones. We have a wide range of products from different categories like fashion jewellery, accessories, home decor, and more.`,
   baseUrl: 'https://myshop.konsoftech.in',
   siteImage: 'https://watermark.lovepik.com/photo/40008/0007.jpg_wh1200.jpg',
